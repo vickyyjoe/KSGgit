e27b8c814eadd53c2bde46fb5638c0948d9c2b6c
vickyyjoe <vickyyjoe12@gmail.com>
2021-02-24 08:54:09 +0700

    Login Test Case

---
+++ Scripts/Login Test Case/Script1614131263811.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://automationpractice.com/index.php')
+
+WebUI.click(findTestObject('Object Repository/Page_My Store/a_Sign in'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Login - My Store/input_Email address_email'), 'abab@abab.com')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Login - My Store/input_Password_passwd'), 'Et4MWeuM9Ns=')
+
+WebUI.click(findTestObject('Object Repository/Page_Login - My Store/span_Sign in'))
+
+WebUI.delay(2)
+
+WebUI.verifyElementVisible(findTestObject('Page_My account - My Store/h1_My account'))
+
+WebUI.closeBrowser()
+
